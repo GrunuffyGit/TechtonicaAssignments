4b7ee27482fef28b1e0225fb1763cc050b79b4f3
GrunuffyGit <Gloria.wm.tam@gmail.com>
2020-02-12 12:42:39 -0800

    local storage implemented into Find/Save. User Log In wip.

--- Assignments/Eventonica/index.js
+++ Assignments/Eventonica/index.js
@@ -26,7 +26,7 @@ class EventRecommender {
     // Adds a new Event to the System
         const convertDate = new Date(eventTime);
         //converting Date
-        const eventObj = new Event(eventId, eventCategory, eventName, convertDate);
+        const eventObj = new Event(eventId, eventCategory.toUpperCase(), eventName, convertDate);
         //create new event obj using event class
         this.AllEvents.push(eventObj);
         //adding the new event obj into the event array
@@ -44,7 +44,7 @@ class EventRecommender {
     // Allow users to save events to a personal Events array.
         const eventToAdd = this.AllEvents.find(eventN => eventN.eventId === eventId);
         //finding the event obj in the event array and storing it into a variable
-        const userIndex = this.AllUsers.findIndex(userN => userN.userName === userName);
+        const userIndex = this.AllUsers.findIndex(userN => userN.userName.toUpperCase() === userName.toUpperCase());
         //finding the user's obj index in the users obj array 
         this.AllUsers[userIndex].userEvents.push(eventToAdd);
         //pushing the event obj into the user's obj's event array by using the found index of the user's obj in AllUsers array
@@ -52,6 +52,7 @@ class EventRecommender {
 
     deleteUser(userName) {
     // Deletes a User from the system
+    userName = userName.toUpperCase();
         let userIndex = this.AllUsers.findIndex(userN => userN.userName === userName);
             // let userIndex = "";
             // for(let i=0; i<this.AllUsers.length;i++){
@@ -67,13 +68,13 @@ class EventRecommender {
         }
     }
    
-    deleteEvent(eventId) {
+    deleteEvent(array, eventId) {
     // Deletes the Event from any event array
-        let eventIndex = this.AllEvents.findIndex(eventN => eventN.eventId === eventId);
+        let eventIndex = array.findIndex(eventN => eventN.eventId === eventId);
         //finding the event obj index in the event obj array
         if(eventIndex > -1){
         //if userId exist
-        this.AllEvents.splice(eventIndex, 1);
+        array.splice(eventIndex, 1);
             //removing 1 element starting from eventIndex #
         }
     }
@@ -86,7 +87,8 @@ class EventRecommender {
         //converting user chosen date
         for(let i=0;i<this.AllEvents.length;i++){
         //loop to go thru all events in AllEvents array
-            if(this.AllEvents[i].eventTime.getTime() === givenDate.getTime()){
+        let eventTime = new Date(this.AllEvents[i].eventTime);
+            if(eventTime.getTime() === givenDate.getTime()){
             //see if time of event matches user given time
                 availableEvents.push(this.AllEvents[i]);
                 //pushing event objs that matches time into avaibleEvents array
@@ -98,6 +100,7 @@ class EventRecommender {
     findEventsbyCategory(category){
     // Returns all events in a given category
         let availableEvents = [];
+        category = category.toUpperCase();
         //empty array to put events that matches the user given category
         for(let i=0;i<this.AllEvents.length;i++){
         //loop to go thru all events in AllEvents array
@@ -110,11 +113,25 @@ class EventRecommender {
         return availableEvents;
     }
 
-    findUserDuplication(inputUN){
-    //find username duplication
+    findUserData(username){
+    //find user's data and return it
+        let userData = [];
+        username = username.toUpperCase();
         for(let i=0;i<this.AllUsers.length;i++){
         //looping through array of all users
-            if(this.AllUsers[i].userName === inputUN){
+            if(this.AllUsers[i].userName.toUpperCase() === username){
+                return userData = this.AllUsers[i].userEvents;
+            }
+        }
+
+    }
+
+    findUserDuplication(inputUN){
+    //find username duplication
+        inputUN = inputUN.toUpperCase();
+        for(let i=0;i<this.AllUsers.length;i++){
+        //looping through array of all users
+            if(this.AllUsers[i].userName.toUpperCase() === inputUN){
             //find if there is a match with user input and existing data
                 return true;
             }
@@ -134,9 +151,10 @@ class EventRecommender {
 
     findEventNameDuplication(inputEventName){
     //find event name dplication
+        inputEventName = inputEventName.toUpperCase();
         for(let i=0;i<this.AllEvents.length;i++){
         //loop through array of all events
-            if(this.AllEvents[i].eventName === inputEventName){
+            if(this.AllEvents[i].eventName.toUpperCase() === inputEventName){
             //find if there is a match with user input and existing data
                 return true;
             }
--- Assignments/Eventonica/jQuery-scripts.js
+++ Assignments/Eventonica/jQuery-scripts.js
@@ -8,7 +8,7 @@ $(document).ready( () => {
 
     //User Management
     $("#all-users").html(showAllUsers(eR.AllUsers));
-    
+
     $("#add-user").submit(function(event){
         event.preventDefault();//prevent reload on submit
         let userName = $("#add-user-name").val();//grabbing UN from input
@@ -44,19 +44,8 @@ $(document).ready( () => {
     //Event Management
     $("#all-events").html(showAllEvents(eR.AllEvents));
     
-    let todayDate = new Date();
-    let mm = todayDate.getMonth() + 1;//getting month add 1 because Jan is 0
-    if(mm<10){ //formatting month if <10
-        mm = "0" + mm;
-    }
-    let dd = todayDate.getDate();//getting date
-    if(dd<10){//formatting date if <10
-        dd = "0" + dd;
-    }
-    let yyyy = todayDate.getFullYear(); //getting year
-    $("#add-event-date").attr("min", `${yyyy}-${mm}-${dd}`);//setting min date to enter(today's date)
-    $("#add-event-date").attr("max", `${yyyy+1}-${mm}-${dd}`);//setting max date to enter(a year after today's date)
-    
+    setMinMaxDate("#add-event-date");//setting Min and Max input for Date 
+
     $("#add-event").submit(function(event){
         $("#event-add-errorMsg").html("");
         event.preventDefault();
@@ -84,8 +73,8 @@ $(document).ready( () => {
         event.preventDefault();//prevent reload on submit
         let eventId = $("#delete-event-id").val();//grab event id
         if(eventId.length !== 0){//find if input is not empty
-            eR.deleteEvent(eventId);//delete event
-            setLocalStorage("Events",eR.AllEvents, "delete");//storing updated array into local storage
+            eR.deleteEvent(eR.AllEvents, eventId);//delete event
+            setLocalStorage("Events", eR.AllEvents, "delete");//storing updated array into local storage
             $("#all-events").html(showAllEvents(eR.AllEvents));//show updated list of events
         }else{
             $("#event-delete-errorMsg").html("Please fill out all field(s)!");
@@ -93,51 +82,80 @@ $(document).ready( () => {
     });
 
     //Find and Save
+    setMinMaxDate("#date-input");//setting Min and Max input for Date 
+
     $("#date-search").submit(function(event){
-        event.preventDefault();
-        let date = $("#date-input").val();
-        $("#date-search-results").html(showAllEvents(eR.findEventsByDate(date)));
+        $("#date-search-errorMsg").html("");//clear error msg
+        event.preventDefault();//prevent reload on submit
+        let date = $("#date-input").val();//grab date
+        if(date.length !== 0){//find if input is empty
+            $("#date-search-results").html(showAllEvents(eR.findEventsByDate(date)));//show results
+        }else{//if input date is empty
+            $("#date-search-errorMsg").html("Please fill out all field(s)!");
+        }
     });
     
     $("#category-search").submit(function(event){
-        event.preventDefault();
-        let category = $("#category-input").val();
-        $("#category-search-results").html(showAllEvents(eR.findEventsbyCategory(category)));
+        $("#category-search-errorMsg").html("");//clear error msg
+        event.preventDefault();//prevent reload on submit
+        let category = $("#category-input").val();//grab category
+        if(category.length !== 0){//find if input is empty
+            $("#category-search-results").html(showAllEvents(eR.findEventsbyCategory(category)));
+        }else{//if input is empty
+            $("#category-search-errorMsg").html("Please fill out all field(s)!");
+        }
     });
     
-    $("#category-search").submit(function(event){
-        event.preventDefault();
-        let userId = $("#save-user-id").val();
-        let eventId = $("#save-event-id").val();
-        $("#user-save-results").html(showAllEvents(eR.saveUserEvent(userId, eventId)));
+    $("#save-user-event").submit(function(event){
+        $("#user-saved-events").html("");//reset user display
+        $("#save-errorMsg").html("");//reset any error msg
+        event.preventDefault();//prevent reload on submit
+        let userName = $("#save-user-name").val();//grab username
+        let eventId = $("#save-event-id").val();//grab event Id
+        eR.saveUserEvent(userName, eventId);//saving event to user
+        setLocalStorage("Users",eR.AllUsers, "User Update");//update local storage
+        $("#user-saved-events").html(`${userName}: <ul>${showAllEvents(eR.findUserData(userName))}</ul>`);//display user data
     });
     console.log(localStorage);
 });
 
 function showAllUsers(array){
+//put all elements in user array into HTML
     let html = "";
-    $.each(array, function(index, user) {
-        html += `<li id="${index}">${user.userName}</li>`;
-    });
+    if(array.length>0){//make sure there is something in array
+        $.each(array, function(index, user) {
+            html += `<li>${user.userName}</li>`;
+        });
+    }else{//if there's nothing in array
+        html = "There are no users currently." 
+    }
     return html;
 }
 
 function showAllEvents(array){
+//put all elements in event array into HTML
     let html = "";
-    $.each(array, function(index, event) {
-        html += `<li>${event.eventName}(${event.eventId})</li>`;
-    });
+    if(array.length>0){//make sure there is something in array
+        $.each(array, function(index, event) {
+            let date = formatTime(event.eventTime);
+            html += `<li>${event.eventName}<ul><li>Event Id: ${event.eventId}</li><li>Category: ${event.eventCategory}</li><li>Date: ${date.month}/${date.day+1}/${date.year}</li></ul></li>`;
+        });
+    }else{//if there's nothing in array
+        html = "There are no events currently."
+    }
     return html;
+    
 }
 
-function setLocalStorage(string,arrayObj, addOrDelete){
-    if(addOrDelete === "add"){
-        if(localStorage.getItem(string) !== null){
-            let currentLS = JSON.parse(localStorage.getItem(string));
-            currentLS = currentLS.concat(arrayObj[arrayObj.length -1]);
-            localStorage.setItem(string, JSON.stringify(currentLS)); 
-        }else{
-           localStorage.setItem(string, JSON.stringify(arrayObj)); 
+function setLocalStorage(string, arrayObj, addOrDelete){
+//setting the local storage
+    if(addOrDelete === "add"){//if adding to the local storage
+        if(localStorage.getItem(string) !== null){ //if local storage is not null
+            let currentLS = JSON.parse(localStorage.getItem(string));//grab current local storage (JSON.parse is to turn local storage back into JSON)
+            currentLS = currentLS.concat(arrayObj[arrayObj.length -1]);//combine current local storage array with last element added to the array 
+            localStorage.setItem(string, JSON.stringify(currentLS));//setting the local storage to the combined array (JSON.stringigy is to turn obj array into a string because local storage stores it as a string)
+        }else{//if local storage is empty
+           localStorage.setItem(string, JSON.stringify(arrayObj));//add array to local storage
         }
     }else{
         localStorage.setItem(string, JSON.stringify(arrayObj)); 
@@ -145,9 +163,36 @@ function setLocalStorage(string,arrayObj, addOrDelete){
 }
 
 function getLocalStorage(string){
-    if(localStorage.getItem(string) !== null){
-        return JSON.parse(localStorage.getItem(string));
-    }else {
-        return [];
+//getting local storage
+    if(localStorage.getItem(string) !== null){//if local storage is not null
+        return JSON.parse(localStorage.getItem(string));//returning obj array of local stoarage (JSON.parse is to turn local storage back into JSON because local storage stores it as a string)
+    }else {//if local storage is empty
+        return [];//return an empty array
     }
+}
+function formatTime(inputDate){
+//returning an obj with time elements to format time
+    let date = new Date(inputDate);
+    let mm = date.getMonth() + 1;//getting month add 1 because Jan is 0
+    if(mm<10){ //formatting month if <10
+        mm = "0" + mm;
+    }
+    let dd = date.getDate();//getting date
+    if(dd<10){//formatting date if <10
+        dd = "0" + dd;
+    }
+    let yyyy = date.getFullYear(); //getting year
+    return{
+        "year" :yyyy,
+        "month" : mm,
+        "day": dd
+    };
+}
+
+function setMinMaxDate(inputDateId){
+//setting Min and Max days for a given HTML element ID
+    let time = new Date();
+    time = formatTime(time);
+    $(`${inputDateId}`).attr("min", `${time.year}-${time.month}-${time.day}`);//setting min date to enter(today's date)
+    $(`${inputDateId}`).attr("max", `${time.year+1}-${time.month}-${time.day}`);//setting max date to enter(a year after today's date)
 }
